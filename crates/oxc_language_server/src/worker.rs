7f103c4f7d12d83a8a32c6b84e1063ab0de9745e
Sysix <sysix@sysix-coding.de>
2026-04-15 22:59:18 +0200

    docs(lsp): document Errors on `run_` worker methods

--- crates/oxc_language_server/src/worker.rs
+++ crates/oxc_language_server/src/worker.rs
@@ -149,6 +149,9 @@ impl WorkspaceWorker {
     }
 
     /// Run different tools to collect diagnostics.
+    ///
+    /// # Errors
+    /// When calling `Tool::run_diagnostic` results into an error.
     pub async fn run_diagnostic(
         &self,
         document: &TextDocument<'_>,
@@ -158,6 +161,9 @@ impl WorkspaceWorker {
     }
 
     /// Run different tools to collect diagnostics on change.
+    ///
+    /// # Errors
+    /// When calling `Tool::run_diagnostic_on_change` results into an error.
     pub async fn run_diagnostic_on_change(
         &self,
         document: &TextDocument<'_>,
@@ -169,6 +175,9 @@ impl WorkspaceWorker {
     }
 
     /// Run different tools to collect diagnostics on save.
+    ///
+    /// # Errors
+    /// When calling `Tool::run_diagnostic_on_save` results into an error.
     pub async fn run_diagnostic_on_save(
         &self,
         document: &TextDocument<'_>,
@@ -183,6 +192,9 @@ impl WorkspaceWorker {
     /// - If the file is not formattable or is ignored, an empty vector is returned
     /// - If the file is formattable, but no changes are made, an empty vector is returned
     /// - If a tool error occurs, an Err is returned
+    ///
+    /// # Errors
+    /// When calling `Tool::run_format` results into an error.
     pub async fn format_file(&self, document: &TextDocument<'_>) -> Result<Vec<TextEdit>, String> {
         let tool_guard = self.tool.read().await;
         let Some(tool) = tool_guard.as_ref() else {
